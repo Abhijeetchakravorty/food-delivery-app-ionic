c4a70d83e2ff87f8f6e7608dc57fd0d44267418e
Abhijeet Chkravorty <chakravorty.abhijeet91@gmail.com>
2021-11-26 13:38:47 +0530

    udpated code for login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,6 +9,9 @@ import {
         styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+        public showError: boolean = false;
+        public showLock: boolean = true;
+        public inputType: string = 'password';
         public user: any = {};
         public showLogin: boolean = true;
         constructor(public router: Router) { }
@@ -29,11 +32,48 @@ export class LoginComponent implements OnInit {
         }
 
         onLogin() {
-
+                // Minimum eight characters, 
+                // at least one uppercase letter, one lowercase letter, 
+                // one number and one special character:
+                console.log('I am here');
+                console.log(this.user);
+                let validate = (this.validateEmail(this.user.email) && this.validatePassword(this.user.password));
+                console.log(validate);
+                if (validate) {
+                        this.showError = false;
+                        this.router.navigate(['/restaurants']);
+                } else {
+                        this.showError = true;
+                        this.showError = true;
+                }
         }
 
         showsignup() {
                 this.showLogin = false;
+        }
+
+        validateEmail(email:any){
+                if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+                        return true;
+                };
+                return false
+        };
+
+        validatePassword(pass: any) {
+                if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/.test(pass)) {
+                        return true;
+                };
+                return false;
+        }
+
+        togglePassword(num: number) {
+                if (this.inputType == 'text') {
+                        this.showLock = true;
+                        this.inputType = 'password';
+                } else {
+                        this.showLock = false;
+                        this.inputType = 'text';
+                }
         }
 }
 
